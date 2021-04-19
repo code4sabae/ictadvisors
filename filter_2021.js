6f096a4e7010439eafc8bdf41e333f5fd6e2d224
Taisuke Fukuno <fukuno@jig.jp>
2021-04-20 03:08:52 +0900

    search with 2020 data

--- filter_2021.js
+++ filter_2021.js
@@ -2,13 +2,38 @@ import { CSV } from "https://code4sabae.github.io/js/CSV.js";
 
 window.onload = async () => {
   const csv = await CSV.fetch("ictadvisors_2021.csv");
+  const csv2020 = await CSV.fetch("ictadvisors_2020.csv");
+  const json = CSV.toJSON(csv2020);
+
+  const search2020 = () => {
+    const key = inputfilter.value;
+    const res = [];
+    for (const d of json) {
+      let flg = false;
+      for (const name in d) {
+        if (d[name].indexOf(key) >= 0) {
+          flg = true;
+          break;
+        }
+      }
+      if (flg) {
+        const idname = d["氏名"];
+        res.push(idname);
+      }
+    }
+    return res;
+  };
 
   inputfilter.onchange = inputfilter.onkeyup = () => {
+    const hitnames = search2020();
+    //console.log(hitnames);
+
     const key = inputfilter.value;
     let cnt = 0;
     for (let i = 1; i < csv.length; i++) {
       const d = csv[i];
-      let flg = false;
+      const name = d[1];
+      let flg = hitnames.indexOf(name) >= 0;
       for (const s of d) {
         if (s.indexOf(key) >= 0) {
           flg = true;
