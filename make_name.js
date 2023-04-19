691b78303af918ab6b11addbc074a558e37ebf03
Taisuke Fukuno <fukuno@jig.jp>
2023-04-20 08:16:04 +0900

    add diff

--- make_name.js
+++ make_name.js
@@ -1,6 +1,20 @@
 import { CSV } from "https://code4sabae.github.io/js/CSV.js";
+//import { JSONUtil } from "https://js.sabae.cc/JSONUtil.js";
+//import { diffChars } from "https://taisukef.github.io/jsdiff-es/src/diff/character.js";
+import { ArrayUtil } from "https://js.sabae.cc/ArrayUtil.js";
 
+const names = {};
 for (let i = 2022; i <= 2023; i++) {
   const data = CSV.toJSON(CSV.decode(await Deno.readTextFile(`ictadvisors_${i}.csv`)));
-  await Deno.writeTextFile(`ictadvisors_name_${i}.csv`, CSV.stringify(data.map(d => ({ 氏名: d.氏名 }))));
+  const name = data.map(d => ({ 氏名: d.氏名 }));
+  await Deno.writeTextFile(`ictadvisors_name_${i}.csv`, CSV.stringify(name));
+  //names[i] = CSV.stringify(name);
+  names[i] = name;
+  //console.log(names[i])
 }
+const diff = ArrayUtil.diff(names[2022], names[2023]);
+console.log(diff, diff.added.length, diff.removed.length);
+await Deno.writeTextFile("ictadvisors_name_diff_2023.json", JSON.stringify(diff, null, 2));
+//console.log(JSON.stringify(JSONUtil.diff(names[2022], names[2023]), null, 2));
+//console.log(JSON.stringify(diffChars(names[2022], names[2023]), null, 2));
+
