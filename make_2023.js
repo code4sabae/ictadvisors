a49676128a48b1fdf238286209e7efabe124c34a
Taisuke Fukuno <fukuno@jig.jp>
2023-04-20 07:34:52 +0900

    add 2023 ver

---
+++ make_2023.js
@@ -0,0 +1,83 @@
+import { CSV } from "https://code4sabae.github.io/js/CSV.js";
+
+const year = 2023;
+
+/*
+try {
+  const url = "https://www.r-ict-advisor.jp/prom/chiiki_adviser/R5/23_ad_opendata_UTF-8&BOM.csv";
+  //const url = "https://www.r-ict-advisor.jp/prom/chiiki_adviser/R4/22_ad_opendata_UTF-8&BOM.csv";
+  const csv = await (await fetch(url)).text();
+  await Deno.writeTextFile(`ictadvisors_${year}.csv`, csv);
+} catch (e) {
+  console.log(e);
+}
+Deno.exit(0);
+*/
+// make htmls
+
+const data = CSV.toJSON(CSV.decode(await Deno.readTextFile(`ictadvisors_${year}.csv`)));
+//const json = CSV.toJSON(csv);
+//console.log(json[0]);
+//Deno.exit(0);
+
+// make index
+const toHTML = (s) => {
+  s = s.replace(/\n/g, "<br>");
+  return s;
+};
+
+// ID,ふりがな,氏名,所属・役職,活動拠点,略歴,地域情報化の専門分野・技術,専門分野,自治体向けメッセージ,関連ＵＲＬ,地域情報化に関する実績（これまでの経験業務・研究活動）,地域情報化に関する実績（これまでに関与した地域情報化に関するプロジェクト）
+const makeHTML = (d) => {
+  // <img style="float:left" src="${d.img}">
+  //<div>${d["所属・役職"]}${d["担当"]}</div>
+  const html1 = `<div id="data${d["ID"]}" class="advisor"><h2>${d["氏名"]}（${d["ふりがな"]}）</h2>`;
+  const names = "所属・役職,活動拠点,略歴,地域情報化の専門分野・技術,専門分野,自治体向けメッセージ,関連ＵＲＬ,地域情報化に関する実績（これまでの経験業務・研究活動）,地域情報化に関する実績（これまでに関与した地域情報化に関するプロジェクト）".split(",");
+  const html2 = [];
+  for (const name of names) {
+    html2.push(`<div><div class=name>${name}</div>${toHTML(d[name])}</div>`);
+  }
+  return html1 + "<div class=content>" + html2.join("<br>") + "</div></div>";
+};
+
+const divs2 = [];
+for (const l of data) {
+  divs2.push(makeHTML(l));
+}
+
+const indexhtml =
+`<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3SZZNGC94H"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3SZZNGC94H');
+</script>
+<meta charset="utf-8">
+<meta name="twitter:card" content="summary_large_image"/>
+<meta property="og:image"  content="https://code4sabae.github.io/ictadvisors/ictadvisors_${year}.jpg">
+<meta name="twitter:image" content="https://code4sabae.github.io/ictadvisors/ictadvisors_${year}.jpg">
+<title>総務省 地域情報化アドバイザー検索 ${year}</title>
+<script type="module" src="filter_${year}.js"></script>
+<link rel="stylesheet" type="text/css" href="index_2022.css">
+<body>
+
+<h1>総務省 地域情報化アドバイザー検索 ${year}</h1>
+<div id=inputc>
+<input id="inputfilter" type=text placeholder="キーワードを入力して絞り込み"><br>
+</div>
+<div id="filtered"></diV>
+
+<div id="main">${divs2.join("\n")}</div>
+<hr>
+<div class=credit>
+  <div>App: <a href=https://fukuno.jig.jp/3920>福野泰介の一日一創</a> (<a href=https://github.com/code4sabae/ictadvisors/>src on GitHub</a>)</div>
+  <div>Data: <a href=https://www.r-ict-advisor.jp/member/opendata/>地域情報化アドバイザーオープンデータ</a> by <a href=https://www.r-ict-advisor.jp>APPLIC（https://www.r-ict-advisor.jp）</a></div>
+  <div>Archive: <a href=index_2022.html>地域情報化アドバイザー一覧2022</a>, <a href=index_2021.html>地域情報化アドバイザー一覧2021</a>, <a href=index_2020.html>地域情報化アドバイザー一覧2020</a></div>
+</div>
+</body>
+</html>`;
+
+await Deno.writeTextFile("index.html", indexhtml);
